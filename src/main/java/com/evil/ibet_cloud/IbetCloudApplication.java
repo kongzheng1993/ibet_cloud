d5a2295004ecf2e1e9a9d04620dc1508c26367a2
kongzheng1993 <kongzheng1993@hotmail.com>
2019-05-28 23:45:11 +0800

    kongzheng 重建项目结构

---
+++ src/main/java/com/evil/ibet_cloud/IbetCloudApplication.java
@@ -0,0 +1,13 @@
+package com.evil.ibet_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbetCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbetCloudApplication.class, args);
+	}
+
+}
