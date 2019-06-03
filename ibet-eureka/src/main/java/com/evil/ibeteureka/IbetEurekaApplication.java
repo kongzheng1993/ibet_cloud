e2d1f9b2e3cc0ce979812dfdac6744c80f26885c
kongzheng1993 <kongzheng1993@hotmail.com>
2019-06-04 00:10:39 +0800

    add eureka

--- ibet-eureka/src/main/java/com/evil/ibeteureka/IbetEurekaApplication.java
+++ ibet-eureka/src/main/java/com/evil/ibeteureka/IbetEurekaApplication.java
@@ -1,13 +1,13 @@
-package com.evil.ibetweb;
+package com.evil.ibeteureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IbetWebApplication {
+public class IbetEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IbetWebApplication.class, args);
+        SpringApplication.run(IbetEurekaApplication.class, args);
     }
 
 }
--- ibet-eureka/src/test/java/com/evil/ibeteureka/IbetEurekaApplicationTests.java
+++ ibet-eureka/src/test/java/com/evil/ibeteureka/IbetEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.evil.ibetweb;
+package com.evil.ibeteureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IbetWebApplicationTests {
+public class IbetEurekaApplicationTests {
 
     @Test
     public void contextLoads() {
