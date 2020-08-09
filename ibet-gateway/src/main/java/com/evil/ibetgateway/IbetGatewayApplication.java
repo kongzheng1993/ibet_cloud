a60753509729916b56cbf45f7e0bb4dc59acd370
kongzheng1993 <kongzheng1993@hotmail.com>
2020-08-09 23:50:57 +0800

    update eureka

--- ibet-gateway/src/main/java/com/evil/ibetgateway/IbetGatewayApplication.java
+++ ibet-gateway/src/main/java/com/evil/ibetgateway/IbetGatewayApplication.java
@@ -1,9 +1,15 @@
 package com.evil.ibetgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class IbetGatewayApplication {
 
     public static void main(String[] args) {
