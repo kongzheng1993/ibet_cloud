282de4e9e17da3284aaf645039ae0f213ebb6e4a
kongzheng1993 <kongzheng1993@hotmail.com>
2020-08-09 14:06:53 +0800

    add gateway

---
+++ ibet-gateway/src/main/java/com/evil/ibetgateway/IbetGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.evil.ibetgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbetGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbetGatewayApplication.class, args);
+    }
+
+}
---
+++ ibet-gateway/src/test/java/com/evil/ibetgateway/IbetGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evil.ibetgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbetGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
